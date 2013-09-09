fc81e973d0ef58edf4c84795e4fd3a5d58b0fce7
Robert Drake <rdrake@direcpath.net>
2013-09-09 02:17:06 -0400

    Integrating the Build.PL Module Builder stuff

---
+++ lib/CIBH.pm
@@ -0,0 +1,9 @@
+package CIBH;
+# this module is for package version only
+use version; $VERSION = qv('1.00');
+1;
+__END__
+
+=head1 NAME
+
+CIBH - Cows in black helicopters
---
+++ t/00.load.t
@@ -0,0 +1,7 @@
+use Test::More tests => 1;
+
+BEGIN {
+use_ok( 'CIBH' );
+}
+
+diag( "Testing CIBH $CIBH::VERSION" );
