28587960775289bd7db6fb47eeb42a69d2004a7a
Robert Drake <rdrake@direcpath.com>
2015-08-10 13:47:33 -0400

    created DS.pm and added load_ds method
    makes it easier to load datastores.  Will add a unit test for this

---
+++ lib/CIBH/DS.pm
@@ -0,0 +1,36 @@
+package CIBH::DS;
+
+use strict;
+use warnings;
+use Module::Load;
+use Carp;
+
+=head1 NAME
+
+CIBH::DS - Module for dealing with datastores
+
+=head1 SYNOPSIS
+
+  use CIBH::DS;
+  my $ds = CIBH::DS::load_ds($opt->{datastore});
+
+=head1 METHODS
+
+=head2 load_ds
+
+    my $ds = CIBH::DS::load_ds($opt->{datastore});
+
+Loads the module for the specified datastore.
+
+=cut
+
+sub load_ds {
+    my $datastore = shift;
+    my $ds = 'CIBH::DS::'. $datastore;
+    eval {
+        Module::Load::load $ds;
+    };
+    croak "Something went wrong with our load of datastore $ds: $@" if ($@);
+}
+
+1;
