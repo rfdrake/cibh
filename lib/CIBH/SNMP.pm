07ca4bd1bf048be5e24f49acb840b26e5fe93819
Robert Drake <rdrake@direcpath.com>
2015-12-03 01:26:04 -0500

    using Net::SNMP->session directly (See #15)

--- lib/CIBH/SNMP.pm
+++ lib/CIBH/SNMP.pm
@@ -24,38 +24,6 @@ Robert Drake, rdrake@cpan.org
 
 =head1 SUBROUTINES
 
-=head2 new
-
-    my $snmp = CIBH::SNMP->new(
-                hostname => $host,
-                community => $community,
-                debug => 1
-    );
-
-Creates a new Net::SNMP session.  Returns the session on success, undef on
-error.  Carps the error message if session fails and debug is set.
-
-We need to make this work with multiple SNMP versions.
-
-=cut
-
-sub new {
-    my $class = shift;
-    my %args = @_;
-
-    my ($snmp, $error) = Net::SNMP->session(
-                  -hostname    => $args{hostname},
-                  -community   => $args{community},
-                  -version     =>  2,
-                  -nonblocking =>  1,
-    );
-
-    if (!defined($snmp)) {
-        carp "Error creating SNMP Session for $args{hostname}: $error\n" if $args{debug};
-    }
-    return $snmp;
-}
-
 =head2 load_snmp_config
 
     my $config = load_snmp_config($host,$opts);
